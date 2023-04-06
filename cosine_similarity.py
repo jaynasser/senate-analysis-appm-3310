8b800ee163f8ed156271274428ed9e29adffac59
jayrnas <jayrnas@icloud.com>
2023-04-06 13:46:15 -0600

    add funtionality to split apart data and visualizations based on party

--- cosine_similarity.py
+++ cosine_similarity.py
@@ -1,11 +1,25 @@
-from senate_data import create_dataframes
+from senate_data import create_senators_df, create_votes_df
 
-import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+
+
+def split_votes_df_by_party(df_votes, df_senators):
+
+	icpsr_to_party = dict(zip(df_senators['icpsr'], df_senators['party_code']))
+
+	df_votes_party = df_votes.copy()
+	df_votes_party['party_code'] = df_votes.icpsr.map(icpsr_to_party)
+
+	df_votes_dem = df_votes_party[df_votes_party['party_code'] == 100]
+	df_votes_rep = df_votes_party[df_votes_party['party_code'] == 200]
+
+	return df_votes_dem, df_votes_rep
+
 
 def create_vote_matrix(df_votes):
-
+	
 	senator_vote_array = df_votes.to_numpy(dtype='int')
 
 	num_rollcalls = len(df_votes.groupby('rollnumber')) # number of votes that occurred
@@ -26,7 +40,7 @@ def create_cosine_similarity_matrix(df_votes):
 	return cos_mat
 
 
-def plot_cosine_similarity_matrix(df_senators, df_votes, matrix, saveout=False, figsize=(30, 30), cmap='hot'):
+def plot_cosine_similarity_matrix(df_senators, df_votes, matrix, saveout=False, saveout_fp='senate_cos_sim.png', figsize=(30, 30), cmap='hot'):
 	
 	icpsr_to_name = dict(zip(df_senators['icpsr'], df_senators['bioname']))
 	plot_labels = np.vectorize(icpsr_to_name.get)(list(df_votes.groupby('icpsr').groups))
@@ -36,6 +50,7 @@ def plot_cosine_similarity_matrix(df_senators, df_votes, matrix, saveout=False, 
 
 	im = ax.matshow(matrix, cmap=cmap)
 	fig.colorbar(im, fraction=0.046, pad=0.04)
+	im.set_clim(0, 1)
 
 	ax.set_xticks(np.arange(len(plot_labels)))
 	ax.set_xticklabels(plot_labels, rotation=90)
@@ -43,17 +58,25 @@ def plot_cosine_similarity_matrix(df_senators, df_votes, matrix, saveout=False, 
 	ax.set_yticklabels(plot_labels)
 
 	if saveout:
-		plt.savefig('senate_cos_sim.png')
+		plt.savefig(saveout_fp)
 
 	plt.show()
 
 
-def cos_sim():
+def cos_sim(df_senators=None, df_votes=None, saveout_plot=False, saveout_fp=['senate_cos_sim_dem.png', 'senate_cos_sim_rep.png']):
 
-	df_senators, df_votes = create_dataframes()
-	cos_mat = create_cosine_similarity_matrix(df_votes)
-	plot_cosine_similarity_matrix(df_senators, df_votes, cos_mat)
+	if df_senators is None:
+		df_senators = create_senators_df()
+
+	if df_votes is None:
+		df_votes = create_votes_df()
+	
+	df_votes_dem, df_votes_rep = split_votes_df_by_party(df_votes, df_senators)
+	
+	cos_mat_dem, cos_mat_rep = create_cosine_similarity_matrix(df_votes_dem), create_cosine_similarity_matrix(df_votes_rep)
+	plot_cosine_similarity_matrix(df_senators, df_votes_dem, cos_mat_dem, saveout=saveout_plot, saveout_fp=saveout_fp[0])
+	plot_cosine_similarity_matrix(df_senators, df_votes_rep, cos_mat_rep, saveout=saveout_plot, saveout_fp=saveout_fp[1])
 
 
 if __name__ == '__main__':
-	cos_sim()
+	cos_sim(saveout_plot=True)
--- senate_data.py
+++ senate_data.py
@@ -24,13 +24,26 @@ def create_votes_df():
 	df_votes.drop(df_votes[df_votes.icpsr.isin(ICPSRS_TO_IGNORE)].index, inplace=True)
 
 	df_votes['cast_code'] = df_votes['cast_code'].map({1: 1, 6: -1, 7: 0, 9: 0}) # reassign cast codes (1 for yea, -1 for nea, 0 for present or abstain)
-
+	
+	present_combinations = set(df_votes.groupby(['rollnumber', 'icpsr']).groups)
+	all_combinations = set()
+	
+	for icpsr in df_votes['icpsr'].unique():
+		for rollnumber in df_votes['rollnumber'].unique():
+			all_combinations.add((rollnumber, icpsr))
+			
+	rollnumbers_to_add, icpsrs_to_add = list(zip(*list(all_combinations - present_combinations)))
+	
+	data_to_add = pd.DataFrame({
+		'icpsr': icpsrs_to_add,
+		'rollnumber': rollnumbers_to_add,
+		'cast_code': [0 for _ in range(len(rollnumbers_to_add))]
+	})
+	
+	df_votes = pd.concat((df_votes, data_to_add))
+	
 	return df_votes
 
 
-def create_dataframes():
-
-	return create_senators_df(), create_votes_df()
-
 if __name__ == '__main__':
-	create_dataframes()
+	df_senators, df_votes = create_senators_df(), create_votes_df()
