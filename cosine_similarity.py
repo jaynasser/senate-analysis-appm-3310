2ee7b4758a12b63056c3a97a571e62ae566fe98f
jayrnas <jayrnas@icloud.com>
2023-04-07 11:06:39 -0600

    attempt to represent votes as relative to party majority (buggy)

--- cosine_similarity.py
+++ cosine_similarity.py
@@ -1,16 +1,26 @@
 from senate_data import create_senators_df, create_votes_df
 
+from typing import Tuple
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 
-def split_votes_df_by_party(df_votes, df_senators):
+def split_votes_df_by_party(df_votes: pd.DataFrame, df_senators: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
 
 	icpsr_to_party = dict(zip(df_senators['icpsr'], df_senators['party_code']))
 
 	df_votes_party = df_votes.copy()
 	df_votes_party['party_code'] = df_votes.icpsr.map(icpsr_to_party)
+
+	df_votes_party = df_votes_party.merge(df_votes_party.groupby(['party_code', 'rollnumber'])['cast_code'].apply(lambda x: x.mode().iloc[0]).rename(index='majority_vote'), how='left', left_on=['party_code', 'rollnumber'], right_index=True)
+	
+	df_votes_party = df_votes_party[df_votes_party['majority_vote'] != 0]
+
+	df_votes_party['vote_relative_to_majority'] = df_votes_party.apply(lambda x: x.cast_code * x.majority_vote, axis=1)
+	
+	df_votes_party = df_votes_party[['rollnumber', 'icpsr', 'vote_relative_to_majority', 'party_code']]
 
 	df_votes_dem = df_votes_party[df_votes_party['party_code'] == 100]
 	df_votes_rep = df_votes_party[df_votes_party['party_code'] == 200]
@@ -18,29 +28,54 @@ def split_votes_df_by_party(df_votes, df_senators):
 	return df_votes_dem, df_votes_rep
 
 
-def create_vote_matrix(df_votes):
+### DEV
+
+def split_votes_df_by_party_old(df_votes, df_senators):
 	
+	icpsr_to_party = dict(zip(df_senators['icpsr'], df_senators['party_code']))
+	
+	df_votes_party = df_votes.copy()
+	df_votes_party['party_code'] = df_votes.icpsr.map(icpsr_to_party)
+	
+	df_votes_dem = df_votes_party[df_votes_party['party_code'] == 100]
+	df_votes_rep = df_votes_party[df_votes_party['party_code'] == 200]
+	
+	return df_votes_dem, df_votes_rep
+
+###
+
+
+def create_vote_matrix(df_votes: pd.DataFrame) -> np.ndarray:
+
 	senator_vote_array = df_votes.to_numpy(dtype='int')
 
 	num_rollcalls = len(df_votes.groupby('rollnumber')) # number of votes that occurred
 
 	vote_matrix = np.reshape(senator_vote_array[: , 2], (num_rollcalls, -1))
-
+	
 	return vote_matrix
 
 
-def create_cosine_similarity_matrix(df_votes):
+def create_cosine_similarity_matrix(df_votes: pd.DataFrame) -> np.ndarray:
 
 	vote_matrix = create_vote_matrix(df_votes)
 
 	d = vote_matrix.T @ vote_matrix
 	norm = (vote_matrix * vote_matrix).sum(0, keepdims=True) ** .5
 	cos_mat = d / norm / norm.T
-	
+
 	return cos_mat
 
 
-def plot_cosine_similarity_matrix(df_senators, df_votes, matrix, saveout=False, saveout_fp='senate_cos_sim.png', figsize=(30, 30), cmap='hot'):
+def plot_cosine_similarity_matrix(
+	df_senators: pd.DataFrame,
+	df_votes: pd.DataFrame,
+	matrix: np.ndarray,
+	saveout: bool = False,
+	saveout_fp: str = 'senate_cos_sim.png',
+	figsize: Tuple[float, float] = (30, 30),
+	cmap: str = 'hot'
+) -> None:
 	
 	icpsr_to_name = dict(zip(df_senators['icpsr'], df_senators['bioname']))
 	plot_labels = np.vectorize(icpsr_to_name.get)(list(df_votes.groupby('icpsr').groups))
@@ -50,7 +85,7 @@ def plot_cosine_similarity_matrix(df_senators, df_votes, matrix, saveout=False, 
 
 	im = ax.matshow(matrix, cmap=cmap)
 	fig.colorbar(im, fraction=0.046, pad=0.04)
-	im.set_clim(0, 1)
+	im.set_clim(0.3, 1)
 
 	ax.set_xticks(np.arange(len(plot_labels)))
 	ax.set_xticklabels(plot_labels, rotation=90)
@@ -63,20 +98,37 @@ def plot_cosine_similarity_matrix(df_senators, df_votes, matrix, saveout=False, 
 	plt.show()
 
 
-def cos_sim(df_senators=None, df_votes=None, saveout_plot=False, saveout_fp=['senate_cos_sim_dem.png', 'senate_cos_sim_rep.png']):
+def cos_sim(
+	df_senators: pd.DataFrame = None,
+	df_votes: pd.DataFrame = None,
+	saveout_plot: bool = False,
+	saveout_fp: Tuple = ('senate_cos_sim_dem.png', 'senate_cos_sim_rep.png')
+) -> None:
 
 	if df_senators is None:
 		df_senators = create_senators_df()
 
 	if df_votes is None:
 		df_votes = create_votes_df()
-	
+
 	df_votes_dem, df_votes_rep = split_votes_df_by_party(df_votes, df_senators)
-	
+
 	cos_mat_dem, cos_mat_rep = create_cosine_similarity_matrix(df_votes_dem), create_cosine_similarity_matrix(df_votes_rep)
 	plot_cosine_similarity_matrix(df_senators, df_votes_dem, cos_mat_dem, saveout=saveout_plot, saveout_fp=saveout_fp[0])
 	plot_cosine_similarity_matrix(df_senators, df_votes_rep, cos_mat_rep, saveout=saveout_plot, saveout_fp=saveout_fp[1])
 
 
 if __name__ == '__main__':
-	cos_sim(saveout_plot=True)
+
+	import sys
+	np.set_printoptions(threshold=sys.maxsize)
+
+#	cos_sim(saveout_plot=True)
+
+	df_senators = create_senators_df()
+	df_votes = create_votes_df()
+
+	df_votes_dem, df_votes_rep = split_votes_df_by_party(df_votes, df_senators)
+	df_votes_dem_old, df_votes_rep_old = split_votes_df_by_party_old(df_votes, df_senators)
+
+	print(create_cosine_similarity_matrix(df_votes_rep) - create_cosine_similarity_matrix(df_votes_rep_old)) # dem metrics should differ because of row dropping. no row drops needed for reps.
